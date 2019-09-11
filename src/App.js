18fe9367de9237dc8237fe81a0a2430a616f95ec
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-09-11 18:56:47 +0300

    Modifies to class. Adds state of app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/common/header";
 import Filters from "./components/filters";
 import Tickets from "./components/tickets";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
-      <main className="app__content">
-        <Filters />
-        <Tickets />
-      </main>
-    </div>
-  );
+class App extends Component {
+  state = {
+    currencies: ["rub", "usd", "eur", "hrn"]
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <main className="app__content">
+          <Filters currencies={this.state.currencies} />
+          <Tickets />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
